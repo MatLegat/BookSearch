5feaeb652366bffbcef2f7c115e1c18df018f19f
Matteus Legat <matteuslegat@gmail.com>
2018-07-22 22:52:46 -0300

    Update functionality + RESTify backend + Book model validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,37 @@
 class BooksController < ApplicationController
 
   def new
+    # form  view
   end
 
-  def show
+  def edit
     @book = Book.find(params[:id])
   end
 
+  def index
+    books = Book.all
+    render json: books
+  end
+
+  def show
+    book = Book.find(params[:id])
+    render json: book
+  end
+
   def create
-    @book = Book.new(book_params)
-    @book.save
-    redirect_to @book
+    book = Book.new(book_params)
+    render json: {
+      'saved' => book.save,
+      'errors' => book.errors
+    }.to_json
+  end
+
+  def update
+    book = Book.find(params[:id])
+    render json: {
+      'updated' => book.update(book_params),
+      'errors' => book.errors
+    }.to_json
   end
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,19 @@
 class Book < ApplicationRecord
+  # requires title:
+  validates :title,
+    presence: true
+  # requires author:
+  validates :author,
+    presence: true
+  # requires description:
+  validates :description,
+    presence: true
+  # requires img and forces it to be a valid URI:
+  validates :img,
+    presence: true,
+    format: {
+      with: URI::regexp(%w(http https)),
+      message: 'must be a valid URI'
+    }
+
 end
